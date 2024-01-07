e6a3f36fd275409d941103492eafea00c6a3eccd
viksun02 <77177153+viksun02@users.noreply.github.com>
2024-01-08 01:37:54 +0200

    adding unit tests

---
+++ test/SimpleSelectionSortTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleSelectionSortTest {
+    static SimpleSelectionSort sorter;
+    static SimpleSelectionSort emptySorter;
+
+    @BeforeAll
+    static void setup() {
+        List<Integer> testList = new ArrayList<>();
+        testList.add(12);
+        testList.add(53);
+        testList.add(102);
+        testList.add(3);
+        testList.add(1);
+        sorter = new SimpleSelectionSort(testList);
+        emptySorter = new SimpleSelectionSort();
+    }
+
+    @Test
+    void givenAnArray_whenSorting_thenGetSortedArray() {
+        sorter.sort();
+        List<Integer> actual = sorter.getArray();
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(3);
+        expected.add(12);
+        expected.add(53);
+        expected.add(102);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    void givenNoArray_whenSorting_thenGetEmptyArray() {
+        emptySorter.sort();
+        assertTrue(emptySorter.getArray().isEmpty());
+    }
+
+}
